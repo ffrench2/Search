b65dc28a54041aefc3a3f78e8e6ea5b2967dbe08
ffrench2 <daragh.ffrench2@mail.dcu.ie>
2022-05-07 23:30:22 +0100

    add story body ui

--- app/src/components/newsStories/newsStories.tsx
+++ app/src/components/newsStories/newsStories.tsx
@@ -9,21 +9,27 @@ export const NewsStories = () => {
       <div className="StoryContainer" data-testid={"StoryTestId"}>
         {storiesResults.map((item: any) => {
           const links = !!item.links && !!item.links.permalink;
+          const images = !!item.media[0] && !!item.media[0].url;
           return (
             <div key={item.id} className="StoryData">
-              <h2 className="StoryTitle">{item.title}</h2>
-              <p className="StoryBody">
-                {!!links ? item.body.substring(0, 450) + " ......" : item.body}
-              </p>
-              {links && (
-                <a
-                  href={item.links.permalink}
-                  target="_blank"
-                  rel="newsStories"
-                >
-                  Click here to read more
-                </a>
-              )}
+              <div className="StoryDataText">
+                <h2 className="StoryTitle">{item.title}</h2>
+                <p className="StoryBody">
+                  {!!links
+                    ? item.body.substring(0, 450) + " ......"
+                    : item.body}
+                </p>
+                {links && (
+                  <a
+                    href={item.links.permalink}
+                    target="_blank"
+                    rel="newsStories"
+                  >
+                    Click here to read more
+                  </a>
+                )}
+              </div>
+              {images && <img src={item.media[0].url} alt="some text" />}
             </div>
           );
         })}
